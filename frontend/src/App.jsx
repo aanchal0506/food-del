cbd0bed0fb90025d4940faccf72f4dc81793d92a
aanchal <ladhaanchal@gmail.com>
2026-01-03 16:19:07 +0530

    home page is ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import Navbar from './components/Navbar/Navbar'
 import Cart from './pages/Cart/Cart'
 import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 import {Route,Routes} from 'react-router-dom'
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar />
       <Routes>
@@ -14,6 +16,8 @@ const App = () => {
         <Route path='/PlaceOrder' element={<PlaceOrder />}/>
       </Routes>
     </div>
+    <Footer />
+    </>
   )
 }
 
