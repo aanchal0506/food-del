09768c42cf36cdefea6eb7148a636246c3ee3284
aanchal <ladhaanchal@gmail.com>
2026-01-03 10:40:22 +0530

    Routes are created and pages are set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import {Route,Routes} from 'react-router-dom'
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/PlaceOrder' element={<PlaceOrder />}/>
+      </Routes>
     </div>
   )
 }
